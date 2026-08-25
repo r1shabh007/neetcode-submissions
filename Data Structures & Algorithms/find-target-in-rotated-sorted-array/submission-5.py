881c1ae2a82f53b03c70559db25680f8b04d4efe
r1shabh007 <54656555+r1shabh007@users.noreply.github.com>
2026-08-25 18:55:01 -0400

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #first find the min number:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2 # floor division
+            if nums[m] < nums[r]: # if right frame is sorted, remove that frame
+                r = m
+            else: # otherwise the right frame has the min, and we need to shorten it
+                l = m + 1
+        m = l
+        l, r = 0, len(nums) - 1
+        while l < r:
+            if target >= nums[m] and target <= nums[r]: # if right frame contains val, remove left frame
+                l = m
+            else: #otherwise left frame contains val and progress frame left
+                r = m - 1
+            m =  -((l + r) // -2) #ceiling division
+        return r if nums[r] == target else -1
