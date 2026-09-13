34092da49dc0dad2151997e1ec30739d66192469
r1shabh007 <54656555+r1shabh007@users.noreply.github.com>
2026-09-13 01:05:08 -0400

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.wordExists = False
+        self.word = "poop"
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        #init trie
+        root = Node()
+        res = set()
+        seen = set()
+        row = len(board)
+        col = len(board[0])
+
+        for word in words:
+            curr = root
+            for i in word:
+                if not i in curr.children:
+                    curr.children[i] = Node()
+                curr = curr.children[i]
+            curr.wordExists = True
+            curr.word = word
+        
+        def dfs(i, j, root):
+            if i < 0 or i > row - 1 or j < 0 or j > col - 1 or (i, j) in seen:
+                return
+
+            nonlocal res
+            curr = root
+            letter = board[i][j]
+            #print(letter, curr.children.keys())
+            if letter in curr.children.keys():
+                prev = curr
+                curr = curr.children[letter]
+                seen.add((i, j))
+                if curr.wordExists:
+                    res.add(curr.word)
+                dfs(i - 1, j, curr)
+                dfs(i + 1, j, curr)
+                dfs(i, j - 1, curr)
+                dfs(i, j + 1, curr)
+                seen.remove((i, j))
+                curr = prev
+            return
+
+        for i in range(row):
+            for j in range(col):
+                curr = root
+                dfs(i, j, root)
+        
+        #print(curr.children["b"].children["a"].children["c"].children["k"].wordExists)
+        return list(res)
+        
+
